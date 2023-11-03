4ccffd0ed77e3277dccc36fc872c54ea65dc7a30
Lobott <129960649+Lobott@users.noreply.github.com>
2023-11-03 12:10:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const btnCoffee = document.getElementById('coffee-btn');
+
+btnCoffee.addEventListener('click', () => {
+    alert("sip it like it's hot 🥵")
+})
+btnCoffee.addEventListener('touchstart', () => {
+    alert("sip it like it's hot 🥵")
+})
