f25d2e6f6b2cdb20d7b9f72fd77ec1d75679a221
Lobott <129960649+Lobott@users.noreply.github.com>
2023-11-07 01:23:41 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 const btnCoffee = document.getElementById('coffee-btn');
+const coffeeB = document.getElementById('coffee-black');
+const container = document.getElementById('count-container');
+const count = document.getElementById('count');
+let coffeeNum = 0;
 
 btnCoffee.addEventListener('click', () => {
-    alert("sip it like it's hot 🥵")
-})
-btnCoffee.addEventListener('touchstart', () => {
-    alert("sip it like it's hot 🥵")
-})
+    coffeeB.style.opacity = '1';
+    setTimeout(() => {
+        coffeeB.classList.add('coffee-animation');
+    }, 500);
+    setTimeout(() => {
+        coffeeB.style.opacity = '0';
+    }, 2000);
+    setTimeout(() => {
+        container.style.opacity = '1';
+    }, 2500);
+    setTimeout(() => {
+        container.style.opacity = '0'
+    }, 4000);
+    setTimeout(() => {
+        coffeeNum++
+        if (coffeeNum <= 10) {
+            count.textContent = coffeeNum;
+        } else {
+            count.textContent = 'SLOW DOWN!'
+        }
+    }, 3000)
+});
+
